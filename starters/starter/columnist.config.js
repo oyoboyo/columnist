02e1cc3d3e4bbe3783ed7e9e048de043a816d782
Sean Rioux <sean@oyoboyo.com>
2022-04-11 15:43:24 -0400

    Default og image

--- starters/starter/columnist.config.js
+++ starters/starter/columnist.config.js
@@ -3,6 +3,7 @@ export const brand = {
   name: "Columnist",
   tagline: "The best content from the world of design",
   logo: "/graphics/logo.svg",
+  image: "/graphics/default.jpg",
   domain: "www.oyoboyo.com",
   contact: "info@oyoboyo.com",
 };

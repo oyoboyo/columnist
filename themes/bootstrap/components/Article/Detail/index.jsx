9c1bbd66f274fe98e6a44d18af93128d0c3eea1e
Sean Rioux <sean@oyoboyo.com>
2022-05-10 16:40:07 -0400

    Pro init

---
+++ themes/bootstrap/components/Article/Detail/index.jsx
@@ -0,0 +1,36 @@
+// core
+import { Html, useConfig } from "@columnist/core";
+// component
+import Head from "./Head";
+import Intro from "./Intro";
+import Author from "../Author";
+
+const Detail = ({ content }) => {
+  const config = useConfig();
+
+  return (
+    <>
+      <Head content={content} />
+      <article className="mb-4">
+        <Intro content={content} />
+        {content.gated ? (
+          <Html className="content-body serif mt-3 ms-md-5 me-md-5">
+            {content.truncated}
+          </Html>
+        ) : content.html ? (
+          <Html className="content-body serif mt-3 ms-md-5 me-md-5">{content.html}</Html>
+        ) : null}
+        {content.author ? (
+          <div className="ms-md-5 me-md-5">
+            <Author
+              content={config.authors ? config.authors[content.author] : content.author}
+              style="teaser"
+            />
+          </div>
+        ) : null}
+      </article>
+    </>
+  );
+};
+
+export default Detail;
